101c8f15c5409d2cb6604e16398d1c4e30de3a99
Agne Nevulyte <agne.nevulyte@finsaeurope.com>
2019-08-02 16:18:08 +0100

    componentDidMount lifecycle added

--- src/index.js
+++ src/index.js
@@ -11,20 +11,31 @@ export default class App extends Component {
             lat: null,
             errorMessage: ""
         }
-
-        window.navigator.geolocation.getCurrentPosition(
-            // callbacks
-            position => {
-                this.setState({
-                    lat: position.coords.latitude
-                })
-            },
-            err => this.setState({
-                errorMessage: err.message
-            })
-        );
     }
     
+
+    // initial data loading
+    componentDidMount() {
+        console.log('my component was rendered to the screen')
+        
+        window.navigator.geolocation.getCurrentPosition(
+            // callbacks
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+
+
+    // data loading when state/props change
+    componentDidUpdate() {
+        console.log('component was updated and rerendered')
+    }
+
+
+    // used to do a cleanup (especially for non-react stuff)
+    componentWillUnmount() {
+
+    }
 
 
     // React says we have to define render!!
@@ -36,7 +47,7 @@ export default class App extends Component {
         if (!this.state.errorMessage && this.state.lat) {
             return <div>Lattitude: {this.state.lat}</div>
         }
-        
+
         return <div>Loading...</div>
     }
 }
