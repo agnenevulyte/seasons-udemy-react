bdd504164da242d6bae06f4b764d3c1aefd0cf67
Agne Nevulyte <agne.nevulyte@finsaeurope.com>
2019-08-02 16:58:35 +0100

    SeasonDisplay getMonth()

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,17 @@
 import React from 'react'
 
-export default function SeasonDisplay() {
+
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+       return lat > 0 ?'summer' : 'winter';
+    } else {
+        return lat > 0 ?'winter' : 'summer';
+    }
+}
+
+export default function SeasonDisplay(props) {
+    const season = getSeason(props.lat, new Date().getMonth())
+    console.log(season)
     return (
         <div>
             ggdrgdfg
